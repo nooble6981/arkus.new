b6d0daca283237fa6d2cd8b7e05b64a764cc8c3c
Artem Vasiliev <nooble6981@gmail.com>
2017-08-07 17:53:43 +0300

    new1

--- mail.php
+++ mail.php
@@ -1,12 +1,12 @@
 <?php
 
-$recepient = "youmail@ya.ru";
-$sitename = "Название сайта";
+$recepient = "nooble6981@gmail.com";
+$sitename = "Arkus";
 
 $name = trim($_GET["name"]);
 $phone = trim($_GET["phone"]);
-$adress = trim($_GET["adress"]);
+$adress = trim($_GET["mark"]);
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
-$message = "Имя: $name \nТелефон: $phone \nАдрес: $adress";
+$message = "Имя: $name \nТелефон: $phone \nМарка: $mark";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
